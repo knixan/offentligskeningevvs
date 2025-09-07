cd1b2d6c94eb22902b29e44c3d5969e1b6bf7921
kristianengelke <e_377@hotmail.com>
2025-09-07 19:57:14 +0200

    Update hero.tsx

--- src/components/hero.tsx
+++ src/components/hero.tsx
@@ -40,7 +40,7 @@ const Hero: React.FC = () => {
       src="/images/skeningebuss2.png"
       alt="Skeninge VVS Rörbuss"
       fill
-      className="h-full object-contain w-full rounded-lg shadow-lg"
+      className="h-full object-contain w-full "
       priority
     />
     {/* Overlay */}
