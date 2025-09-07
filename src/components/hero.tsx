064f346d2cbbfe74b52d9e73f54ec5705e6d98fc
kristianengelke <e_377@hotmail.com>
2025-09-07 18:07:56 +0200

    Update hero.tsx

--- src/components/hero.tsx
+++ src/components/hero.tsx
@@ -34,39 +34,38 @@ const Hero: React.FC = () => {
       </div>
 
       {/* Desktop */}
-      <div className="hidden md:flex h-screen justify-center items-start bg-background">
-        <div className="relative h-full w-full">
-          <Image
-            src="/images/skeningebuss2.png"
-            alt="Skeninge VVS Rörbuss"
-            fill
-            style={{ objectFit: "cover" }}
-            className="w-full h-full"
-            priority
-          />
-          {/* Overlay */}
-          <div className="absolute inset-0 bg-black/30" />
+ <div className="hidden md:flex h-screen justify-center items-center bg-opacity-100">
+  <div className="relative h-full w-full">
+    <Image
+      src="/images/skeningebuss2.png"
+      alt="Skeninge VVS Rörbuss"
+      fill
+      className="h-full object-contain w-full"
+      priority
+    />
+    {/* Overlay */}
+    <div className="absolute inset-0 bg-black/30" />
 
-          {/* Text */}
-          <div className="absolute inset-0 flex flex-col items-center justify-start pt-10 text-center">
-            <h1 className="mb-4 text-6xl font-extrabold tracking-tight text-red-700">
-              Din lokala expert på VVS!
-            </h1>
-            <p className="mb-6 text-4xl font-extrabold text-sky-600">
-              Små i storlek – stora på service
-            </p>
-            <Button asChild>
-              <Link
-                href="#contact"
-                className="inline-flex items-center bg-red-800 hover:scale-105  hover:bg-red-900 transition-transform duration-200"
-              >
-                Kontakta oss
-                <ArrowRight className="ml-2 h-4 w-4" />
-              </Link>
-            </Button>
-          </div>
-        </div>
-      </div>
+    {/* Text */}
+    <div className="absolute inset-0 flex flex-col items-center justify-start pt-10 text-center">
+      <h1 className="mb-4 text-6xl font-extrabold tracking-tight text-red-700">
+        Din lokala expert på VVS!
+      </h1>
+      <p className="mb-6 text-4xl font-extrabold text-gray-100">
+        Små i storlek – stora på service
+      </p>
+      <Button asChild>
+        <Link
+          href="#contact"
+          className="inline-flex items-center bg-red-800 hover:scale-105 hover:bg-red-900 transition-transform duration-200"
+        >
+          Kontakta oss
+          <ArrowRight className="ml-2 h-4 w-4" />
+        </Link>
+      </Button>
+    </div>
+  </div>
+</div>
     </>
   );
 };
